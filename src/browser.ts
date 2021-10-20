d8e8adf98960eb202cb234d148a9a3530fd7b86d
Adrien MILLE <adrien.mille.aer@gmail.com>
2021-10-20 14:34:15 +0800

    feat: use a singleton to launch a single browser

---
+++ src/browser.ts
@@ -0,0 +1,43 @@
+import puppeteer from 'puppeteer';
+
+let instance: puppeteer.Browser | null = null;
+
+let instancePromise: Promise<puppeteer.Browser> | null = null;
+
+const launchBrowser = async (): Promise<puppeteer.Browser> =>
+    puppeteer.launch({
+        headless: true,
+        args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+export const getBrowser = () => {
+    if (instance) {
+        // browser already available
+        return instance;
+    }
+
+    if (instancePromise) {
+        // browser currently starting
+        return instancePromise;
+    }
+
+    // start initializing and update the local variable
+    instancePromise = launchBrowser();
+
+    // then return the promise we just created
+    return instancePromise.then(browser => {
+        // update the local variable
+        instance = browser;
+
+        // then return it
+        return browser;
+    });
+};
+
+export const closeBrowser = () => {
+    if (!instance) {
+        return Promise.resolve();
+    }
+
+    return instance.close();
+};
