704a8130d2f9d00de7740bbd093f830f900d386f
Adrien MILLE <adrien.mille.aer@gmail.com>
2022-06-09 14:29:52 +0800

    chore: update release config to trigger on dependencies

--- release.config.js
+++ release.config.js
@@ -6,7 +6,16 @@ module.exports = {
         },
     ],
     plugins: [
-        '@semantic-release/commit-analyzer',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                releaseRules: [
+                    { type: 'docs', scope: 'readme', release: 'patch' },
+                    { type: 'chore', scope: 'deps', release: 'patch' },
+                ],
+            },
+        ],
         '@semantic-release/release-notes-generator',
         '@semantic-release/github',
     ],
